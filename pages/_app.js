40280d3d757a6e0bd225e256d17fc5cfa2aa9207
Paul Scanlon <pauliescanlon@gmail.com>
2024-07-19 14:15:25 +0100

    feat: add page header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import React, { Fragment } from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
 
 import { IBM_Plex_Sans } from 'next/font/google';
+
+import logo from '../static/logo.svg';
+import GitHubIcon from '../components/github-icon';
 
 import '../styles/globals.css';
 
@@ -46,6 +50,31 @@ const App = ({ Component, pageProps }) => {
         {/* favicon */}
         <link rel='icon' type='image/png' href={`${url}/static/favicon.png`} />
       </Head>
+
+      <header className='absolute top-0 left-0 w-full h-16 z-20'>
+        <nav className='flex justify-between px-4 xl:px-12 py-5 w-full mx-auto max-w-8xl'>
+          <a
+            href='https://neon.tech/'
+            target='_blank'
+            rel='noopener'
+            className='self-start'
+            // onClick={() => sendGtagEvent('click_twin_thing_neon_logo')}
+          >
+            <Image priority src={logo} alt='Neon logo' className='w-[124px] m-0' />
+          </a>
+          <a
+            href='https://github.com/neondatabase/twin-thing'
+            target='_blank'
+            rel='noopener'
+            className='flex gap-2 text-white font-bold self-start border-2 border-white items-center p-1 xl:p-1.5 -mt-1 rounded-full 
+              no-underline transition-colors duration-300 hover:border-brand-primary'
+            // onClick={() => sendGtagEvent('click_twin_thing_repository')}
+          >
+            <GitHubIcon className='w-7 h-7 xl:h-8 xl:w-8' />
+            <span className='hidden xl:block pr-4 pb-1'>twin-thing</span>
+          </a>
+        </nav>
+      </header>
 
       <main className={`prose max-w-none ${ibmPlexSans.variable}`}>
         <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 const Page = () => {
-  return <h1>Twin Thing</h1>;
+  return (
+    <div className='relative bg-brand-background pb-16 xl:pb-24 mx-auto max-w-8xl'>
+      <div className='relative pt-8 pb-16 xl:py-4 mx-auto max-w-6xl'>
+        <section></section>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
